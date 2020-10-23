c223c777c118bc7d09988efea664111c3f71687f
Chase Ballard <44304471+itschoot@users.noreply.github.com>
2020-10-22 21:59:37 -0400

    Create kornislav.cpp

---
+++ kornislav.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int side[4];
+
+    cin >> side[0] >> side[1] >> side[2] >> side[3];
+
+    sort(side, side + 4);
+
+    cout << (side[0] * side[2]);
+    
+    return 0;
+}
