239c4e03b660729fd2472ecb6a2cc764796f28b3
Chase Ballard <44304471+itschoot@users.noreply.github.com>
2020-10-22 20:28:19 -0400

    Create datum.cpp

---
+++ datum.cpp
@@ -0,0 +1,261 @@
+#include <iostream>
+using namespace std;
+
+int d, m;
+
+int main() {
+    cin >> d >> m;
+    if (m == 1) {
+        if (d % 7 == 1) {
+            cout << "Thursday";
+        }
+        else if (d % 7 == 2) {
+            cout << "Friday";
+        }
+        else if (d % 7 == 3) {
+            cout << "Saturday";
+        }
+        else if (d % 7 == 4) {
+            cout << "Sunday";
+        }
+        else if (d % 7 == 5) {
+            cout << "Monday";
+        }
+        else if (d % 7 == 6) {
+            cout << "Tuesday";
+        }
+        else cout << "Wednesday";
+    }
+    else if (m == 2) {
+        if (d % 7 == 1) {
+            cout << "Sunday";
+        }
+        else if (d % 7 == 2) {
+            cout << "Monday";
+        }
+        else if (d % 7 == 3) {
+            cout << "Tuesday";
+        }
+        else if (d % 7 == 4) {
+            cout << "Wednesday";
+        }
+        else if (d % 7 == 5) {
+            cout << "Thursday";
+        }
+        else if (d % 7 == 6) {
+            cout << "Friday";
+        }
+        else cout << "Saturday";
+    }
+    else if (m == 3) {
+        if (d % 7 == 1) {
+            cout << "Sunday";
+        }
+        else if (d % 7 == 2) {
+            cout << "Monday";
+        }
+        else if (d % 7 == 3) {
+            cout << "Tuesday";
+        }
+        else if (d % 7 == 4) {
+            cout << "Wednesday";
+        }
+        else if (d % 7 == 5) {
+            cout << "Thursday";
+        }
+        else if (d % 7 == 6) {
+            cout << "Friday";
+        }
+        else cout << "Saturday";
+    }
+    else if (m == 4) {
+        if (d % 7 == 1) {
+            cout << "Wednesday";
+        }
+        else if (d % 7 == 2) {
+            cout << "Thursday";
+        }
+        else if (d % 7 == 3) {
+            cout << "Friday";
+        }
+        else if (d % 7 == 4) {
+            cout << "Saturday";
+        }
+        else if (d % 7 == 5) {
+            cout << "Sunday";
+        }
+        else if (d % 7 == 6) {
+            cout << "Monday";
+        }
+        else cout << "Tuesday";
+    }
+    else if (m == 5) {
+        if (d % 7 == 1) {
+            cout << "Friday";
+        }
+        else if (d % 7 == 2) {
+            cout << "Saturday";
+        }
+        else if (d % 7 == 3) {
+            cout << "Sunday";
+        }
+        else if (d % 7 == 4) {
+            cout << "Monday";
+        }
+        else if (d % 7 == 5) {
+            cout << "Tuesday";
+        }
+        else if (d % 7 == 6) {
+            cout << "Wednesday";
+        }
+        else cout << "Thursday";
+    }
+    else if (m == 6) {
+        if (d % 7 == 1) {
+            cout << "Monday";
+        }
+        else if (d % 7 == 2) {
+            cout << "Tuesday";
+        }
+        else if (d % 7 == 3) {
+            cout << "Wednesday";
+        }
+        else if (d % 7 == 4) {
+            cout << "Thursday";
+        }
+        else if (d % 7 == 5) {
+            cout << "Friday";
+        }
+        else if (d % 7 == 6) {
+            cout << "Saturday";
+        }
+        else cout << "Sunday";
+    }
+    else if (m == 7) {
+        if (d % 7 == 1) {
+            cout << "Wednesday";
+        }
+        else if (d % 7 == 2) {
+            cout << "Thursday";
+        }
+        else if (d % 7 == 3) {
+            cout << "Friday";
+        }
+        else if (d % 7 == 4) {
+            cout << "Saturday";
+        }
+        else if (d % 7 == 5) {
+            cout << "Sunday";
+        }
+        else if (d % 7 == 6) {
+            cout << "Monday";
+        }
+        else cout << "Tuesday";
+    }
+    else if (m == 8) {
+        if (d % 7 == 1) {
+            cout << "Saturday";
+        }
+        else if (d % 7 == 2) {
+            cout << "Sunday";
+        }
+        else if (d % 7 == 3) {
+            cout << "Monday";
+        }
+        else if (d % 7 == 4) {
+            cout << "Tuesday";
+        }
+        else if (d % 7 == 5) {
+            cout << "Wednesday";
+        }
+        else if (d % 7 == 6) {
+            cout << "Thursday";
+        }
+        else cout << "Friday";
+    }
+    else if (m == 9) {
+        if (d % 7 == 1) {
+            cout << "Tuesday";
+        }
+        else if (d % 7 == 2) {
+            cout << "Wednesday";
+        }
+        else if (d % 7 == 3) {
+            cout << "Thursday";
+        }
+        else if (d % 7 == 4) {
+            cout << "Friday";
+        }
+        else if (d % 7 == 5) {
+            cout << "Saturday";
+        }
+        else if (d % 7 == 6) {
+            cout << "Sunday";
+        }
+        else cout << "Monday";
+    }
+    else if (m == 10) {
+        if (d % 7 == 1) {
+            cout << "Thursday";
+        }
+        else if (d % 7 == 2) {
+            cout << "Friday";
+        }
+        else if (d % 7 == 3) {
+            cout << "Saturday";
+        }
+        else if (d % 7 == 4) {
+            cout << "Sunday";
+        }
+        else if (d % 7 == 5) {
+            cout << "Monday";
+        }
+        else if (d % 7 == 6) {
+            cout << "Tuesday";
+        }
+        else cout << "Wednesday";
+    }
+    else if (m == 11) {
+        if (d % 7 == 1) {
+            cout << "Sunday";
+        }
+        else if (d % 7 == 2) {
+            cout << "Monday";
+        }
+        else if (d % 7 == 3) {
+            cout << "Tuesday";
+        }
+        else if (d % 7 == 4) {
+            cout << "Wednesday";
+        }
+        else if (d % 7 == 5) {
+            cout << "Thursday";
+        }
+        else if (d % 7 == 6) {
+            cout << "Friday";
+        }
+        else cout << "Saturday";
+    }
+    else if (m == 12) {
+        if (d % 7 == 1) {
+            cout << "Tuesday";
+        }
+        else if (d % 7 == 2) {
+            cout << "Wednesday";
+        }
+        else if (d % 7 == 3) {
+            cout << "Thursday";
+        }
+        else if (d % 7 == 4) {
+            cout << "Friday";
+        }
+        else if (d % 7 == 5) {
+            cout << "Saturday";
+        }
+        else if (d % 7 == 6) {
+            cout << "Sunday";
+        }
+        else cout << "Monday";
+    }
+    return 0;
+}
